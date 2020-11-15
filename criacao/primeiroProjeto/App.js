7d7bccb27c6ac0f388a9d10ae1e2437c0f1e3d65
Andreia Pasqual <pasqual1982.ap@gmail.com>
2020-11-15 15:56:43 -0300

    so comecndo

--- criacao/primeiroProjeto/App.js
+++ criacao/primeiroProjeto/App.js
@@ -1,19 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{Component} from 'react';
-import { Text, View } from 'react-native';
-import C1 from 'componentes/comp1.js';
+import { Text, View, StyleSheet } from 'react-native'; 
+import C1  from './componentes/comp1'
+
 
 
 export default function App1(){
   return(
-    <View>
-      <C1/>
+    <View style={estilo.conteiner}>
+      <C1 dia="segunda" mes="11"  ano="2020" cor="#ff0"/>
+      <C1 dia="terca" mes="11" ano="2020" cor="f0f"/>
+      <C1 dia="quarta"  mes="11"ano="2020"cor="#0ff" />
+      <C1 dia="quinta"  mes="11" ano="2020" cor="000"/>
+      <Text style={estilo.txt0}>vamo que vamo</Text>
+      <Text style={{color:'#f00', fontSize:30}}>cada dia aprendo mais</Text>
     </View>
   );
 };
-
-
-
+const estilo = StyleSheet.create({
+  conteiner:{
+    flex: 1,
+    backgroundColor:'#aaa',
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  txt0:{
+      color: '#080',
+      fontSize: 10,
+  },
+})
+/* propriedades: style e uma propriedade tambem, 
+/* formatacao inline enecessario duas chaves*/
 /*Podemos ulilizar tanto o componente de classe como o de função, ma e ncessario faser a importacao do
  componente podemos utilizar os dois dependendo das necessidades do projeto, o babel e o responsvelpela 
 traduação do jsx para o conteudo nativo da pratforma do aplicativo nativo ios ou android*/
--- criacao/primeiroProjeto/componentes/comp1.js
+++ criacao/primeiroProjeto/componentes/comp1.js
@@ -1,10 +1,30 @@
-import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text  } from 'react-native';
+import React from 'react';
+import {StyleSheet, Text } from 'react-native';
 
-export default function Comp1(){
+let an;
+let cor;
+export default function(props) {
+    an= props.ano
+    cor=props.cor
     return(
-      <Text>viva</Text>
+    <Text style={estilo.txt2, {color:cor}}>Eu consegui na {props.dia} mes={props.mes} ano={an} </Text>
     )
-  }
-  
+}
+
+const estilo = StyleSheet.create({
+    txt1:{
+        //color: '#f00',
+        fontSize: 20,
+    },
+    txt2:{
+        //color: '#00f',
+        fontSize: 15,
+    }
+});
+
+
+
+
+/* Onde eu precisar posso utolizar esse componente inclsive dentro de outro componente ou eviar
+ informacao de pai para filhoou de filho para pai*/
