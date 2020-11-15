63a2c8984e45e9dae1eae0ec3829c62c3cc474c7
Andreia Pasqual <pasqual1982.ap@gmail.com>
2020-11-15 14:12:31 -0300

    add

--- criacao/primeiroProjeto/App.js
+++ criacao/primeiroProjeto/App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React,{Component} from 'react';
 import { Text, View } from 'react-native';
+import C1 from 'componentes/comp1.js';
 
-export default function App1() {
-  return (
+
+export default function App1(){
+  return(
     <View>
-      <Text>Vamos la agora eu consegui</Text>
+      <C1/>
     </View>
   );
 };
 
 
+
+/*Podemos ulilizar tanto o componente de classe como o de função, ma e ncessario faser a importacao do
+ componente podemos utilizar os dois dependendo das necessidades do projeto, o babel e o responsvelpela 
+traduação do jsx para o conteudo nativo da pratforma do aplicativo nativo ios ou android*/
+
+/* criamos um componente e podemos reaproveila-lo em varios momentos entro do aplicativo*/
+
+/*export default class app1 extends Component{
+  render(){
+    return(
+      <View>
+      <View>
+        <Text>Vamos la agora eu consegui</Text>
+      </View>
+     <View>
+      <Text>Vamos la agora eu consegui</Text>
+      <Text>Vamos la agora eu consegui</Text>
+    </View>
+    </View>
+    );
+  }
+}*/
---
+++ criacao/primeiroProjeto/componentes/comp1.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Text  } from 'react-native';
+
+export default function Comp1(){
+    return(
+      <Text>viva</Text>
+    )
+  }
+  
